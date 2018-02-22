3600dbfe0d8440a8d463e3eee24b55a1aca19c7f
Jesus Vasquez <jvasquez@slac.stanford.edu>
2018-02-22 15:20:04 -0800

    Add scale factors

--- iocBoot/sioc-li00-mp01/st.cmd
+++ iocBoot/sioc-li00-mp01/st.cmd
@@ -104,6 +104,12 @@ asynSetTraceMask("${YCPSWASYN_PORT}",, -1, 0)
 #               DB LOADING
 # ===========================================
 dbLoadRecords("db/mpsLN.db", "P=${PREFIX_MPS_BASE}, PORT=${YCPSWASYN_PORT}")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY0}, R=C0I0_FWSCL, EGU=, PREC=4, VAL=1")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY0}, R=C1I0_FWSCL, EGU=, PREC=4, VAL=1")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY0}, R=C2I0_FWSCL, EGU=, PREC=4, VAL=1")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY1}, R=C0I0_FWSCL, EGU=, PREC=4, VAL=1")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY1}, R=C1I0_FWSCL, EGU=, PREC=4, VAL=1")
+dbLoadRecords("db/scale.db", "P=${PREFIX_MPS_BAY1}, R=C2I0_FWSCL, EGU=, PREC=4, VAL=1")
 dbLoadRecords("db/saveLoadConfig.db", "P=${PREFIX_MPS_BASE}, PORT=${YCPSWASYN_PORT}")
 
 # **********************************************************************
