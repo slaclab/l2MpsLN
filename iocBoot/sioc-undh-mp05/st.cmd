de1927ad99d4401b1a3c176017ec6767324bad31
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2019-07-11 12:33:18 -0700

    Missing edm panels

--- iocBoot/sioc-undh-mp05/st.cmd
+++ iocBoot/sioc-undh-mp05/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linuxRT-x86_64/l2MpsLN
+#!iocSpecificRelease/bin/linuxRT-x86_64/l2MpsLN
 #
 # SIOC:UNDH:MP05
 #
@@ -21,7 +21,7 @@ epicsEnvSet("CPU_NAME", "cpu-undh-sp02")
 epicsEnvSet("CRATE_ID", "0001")
 epicsEnvSet("SLOT_ID", "2")
 epicsEnvSet("LN_CONFIG_TOP", "${CONFIG_TOP}/${CPU_NAME}/${CRATE_ID}/0${SLOT_ID}")
-epicsEnvSet("SLOT_ID", "7") # This is later used to set the LN card IP address (testing it in slot 7)
+#epicsEnvSet("SLOT_ID", "7") # This is later used to set the LN card IP address (testing it in slot 7)
 
 #
 # Loads generated mps environment variables for this SIOC, variables:
