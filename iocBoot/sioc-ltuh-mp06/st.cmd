a5a8e429277f744c15df8632abcf5857d092f60e
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2019-09-03 14:16:35 -0700

    Added iocBoot folders for NC link nodes

---
+++ iocBoot/sioc-ltuh-mp06/st.cmd
@@ -0,0 +1,15 @@
+#!iocSpecificRelease/bin/linuxRT-x86_64/l2MpsLN
+#
+# SIOC:LTUH:MP06
+#
+
+< envPaths
+
+epicsEnvSet("SLOT_ID", "2")
+epicsEnvSet("SLOT_ID", "6") # This is later used to set the LN card IP address (testing it in slot 7)
+epicsEnvSet("FPGA_IP","10.0.1.10${SLOT_ID}")
+
+#
+# Loads common Link Node startup
+#
+< ${TOP}/iocBoot/common/link_node.cmd
