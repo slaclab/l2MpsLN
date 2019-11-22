8b9ea29872db90d308d18de38ff39136f99ad2a2
Jesus Vasquez <jvasquez@slac.stanford.edu>
2019-11-22 01:01:12 -0800

    Move the LCLS1 BSA data from the data stream to the BsaCore facility

--- l2MpsLNApp/src/lcls1_bsa.cpp
+++ l2MpsLNApp/src/lcls1_bsa.cpp
@@ -3,32 +3,129 @@
 
 ReadStream::ReadStream(const std::string& streamName, const std::string& recordPrefix)
 :
+    // This are the parameters names for each of the 24 data words in the data stream.
+    // The order here should match the order of the data words in the stream, as defined 
+    // in the FW application.
+    // The BSA PV name will be '${recordPrefix}:' followed by this parameter name.
+    bsaChannelNames{
+        "LC1_BSA_00",
+        "LC1_BSA_01", 
+        "LC1_BSA_02", 
+        "LC1_BSA_03", 
+        "LC1_BSA_04", 
+        "LC1_BSA_05", 
+        "LC1_BSA_06", 
+        "LC1_BSA_07", 
+        "LC1_BSA_08", 
+        "LC1_BSA_09", 
+        "LC1_BSA_10", 
+        "LC1_BSA_11", 
+        "LC1_BSA_12", 
+        "LC1_BSA_13", 
+        "LC1_BSA_14", 
+        "LC1_BSA_15", 
+        "LC1_BSA_16", 
+        "LC1_BSA_17", 
+        "LC1_BSA_18", 
+        "LC1_BSA_19", 
+        "LC1_BSA_20", 
+        "LC1_BSA_21", 
+        "LC1_BSA_22", 
+        "LC1_BSA_23", 
+    },
+    bsaChannels(createBsaChannels(recordPrefix, bsaChannelNames)),
     strm_(IStream::create(cpswGetRoot()->findByName(streamName.c_str()))),
     streamThread_( std::thread( &ReadStream::streamTask, this) )
 {
-    std::cout << "Read Stream object created" << std::endl;
-    std::cout << "record prefix: " << recordPrefix << std::endl;
+    // Set the name for the 'streamThread_' thread
+    if( pthread_setname_np( streamThread_.native_handle(), "L2MpsLcls1Bsa" ) )
+        std::cerr <<  "pthread_setname_np failed for L2MpsL1Bsa thread" << std::endl;
 }
 
 ReadStream::~ReadStream()
 {
-    std::cout << "Read Stream object destroyed" << std::endl;
 }
 
 void ReadStream::streamTask()
 {
-    uint8_t *buf = new uint8_t[10000];
+    uint8_t *buf = new uint8_t[500];
     std::size_t got;
-    std::cout << "streamTask thread created" << std::endl;
 
+    //std::size_t count {0};
     for(;;)
     {
-        //std::cout << "  ... waiting stream data..." << std::endl;
+        // Read the data stream. This call is blocking with a 1s timeout
         got = strm_->read( buf, 10000, CTimeout(1000000) );
+        
+        // Check if the stream read timed out
+        if ( 0 == got )
+            continue;
 
-        //std::cout << "   streamTask: received " << got << " bytes." << std::endl;
+        // If we received data, check that we received the correct number of bytes
+        if ( sizeof(stream_data_t) != got )
+        {
+            std::cerr << "ERROR: LCLS1 BSA Stream: bad size. Received " << got << ", instead of " << sizeof(stream_data_t) << " bytes" << std::endl;
+            continue;
+        }
 
+        // Create a stream data pointer on the received data buffer
+        stream_data_t* pSD{ (stream_data_t*)buf };
+
+        // Temporal fix: the FW app is swapping the word
+        // order of the timestamp, so let do a swap in SW
+        // for now
+        epicsTimeStamp etimeStamp;
+        etimeStamp.secPastEpoch = pSD->timeStamp_H;
+        etimeStamp.nsec = pSD->timeStamp_L;
+
+        // Copy the LCLS1 BSA data from the stream into the BsaCore facility
+        for (std::size_t i{0}; i < bsaChannelNames.size(); ++i)
+        {
+            BSA_StoreData(
+                bsaChannels.at(i), 
+                etimeStamp, //pSD->timeStamp, 
+                pSD->data[i],
+                epicsAlarmNone,  // For now, we don't support alarm
+                epicsSevNone);   // nor severity.
+        }
+
+        // This is temporal debug feature. It prints out 
+        // 1/360 frames into the IOC shell
+        //if (++count >= 359)
+        //{
+        //    count = 0;
+
+        //    std::cout << "Stream data:" << std::endl;
+        //    std::cout << "===================" << std::endl;
+        //    std::cout << std::hex;
+        //    std::cout << "Header     = 0x" <<  pSD->header    << std::endl;
+        //    std::cout << "Time stamp = 0x" <<  etimeStamp.secPastEpoch << ", " << etimeStamp.nsec << std::endl;
+        //    for (std::size_t i{0}; i < 6; ++i)
+        //        std::cout << "DMOD       = 0x" <<  pSD->dmod[i] << std::endl;
+        //    std::cout << "edefInit   = 0x" <<  pSD->edefInit  << std::endl;
+        //    std::cout << "edefMajor  = 0x" <<  pSD->edefMajor << std::endl;
+        //    std::cout << "edefMinor  = 0x" <<  pSD->edefMinor << std::endl;
+        //    std::cout << "edefAvgDn  = 0x" <<  pSD->edefAvgDn << std::endl;
+        //    for (std::size_t i{0}; i < 24; ++i)
+        //        std::cout << "DATA       = 0x" <<  pSD->data[i] << std::endl;
+        //    std::cout << std::dec;
+        //    std::cout << "===================" << std::endl;
+        //    std::cout << std::endl;
+        //}
     }
+}
+
+std::vector<BsaChannel> ReadStream::createBsaChannels(const std::string& prefix, const std::vector<std::string>& names)
+{
+    std::vector<BsaChannel> temp;
+    for (auto it = names.begin(); it != names.end(); ++it)
+    {
+        std::string id ( prefix + ":" + *it );
+        std::cout << "  Creating BSA channel for: " << id << std::endl;
+        temp.push_back(BSA_CreateChannel(id.c_str()));
+    }
+
+    return temp;
 }
 
 extern "C" int Lcls1BsaConfig(const char* streamName, const char* recordPrefix)
--- l2MpsLNApp/src/lcls1_bsa.h
+++ l2MpsLNApp/src/lcls1_bsa.h
@@ -7,6 +7,7 @@
 #include <thread>
 #include <iocsh.h>
 #include <epicsExport.h>
+#include <alarm.h>
 #include <errlog.h>
 #include <yamlLoader.h>
 #include <cpsw_api_user.h>
@@ -19,9 +20,38 @@ public:
     ~ReadStream();
 
 private:
-    Stream      strm_;
-    std::thread streamThread_;
-    void        streamTask();
+    // Factory method to crete all the vector of BSA channels based on the vector of channel names.
+    static std::vector<BsaChannel> createBsaChannels(const std::string& prefix, const std::vector<std::string>& names);
+
+    // Structure of the stream data.
+    // This structure must be packed and byte
+    // aligned to match the firmware stream
+    typedef struct 
+    {
+        uint32_t header;
+        // Temporal fix: the FW app is swapping the word 
+        // order of the timestamp, so let do a swap in SW 
+        // for now
+        uint32_t timeStamp_L; //epicsTimeStamp timeStamp;
+        uint32_t timeStamp_H;
+        uint32_t dmod[6];
+        uint32_t edefInit;
+        uint32_t edefMajor;
+        uint32_t edefMinor;
+        uint32_t edefAvgDn;
+        uint32_t data[24];
+    } 
+    __attribute__((packed, aligned(1)))
+    stream_data_t;
+
+    // This method receive the dat stream, process it, and move the data
+    // to BsaCore. It runs in the streamThread_ thread.
+    void streamTask();
+
+    const std::vector<std::string> bsaChannelNames; // BSA channel names
+    std::vector<BsaChannel>        bsaChannels;     // BSA channels
+    Stream                         strm_;           // Firmware data stream interface
+    std::thread                    streamThread_;   // Stream receiver thread
 };
 
 #endif
