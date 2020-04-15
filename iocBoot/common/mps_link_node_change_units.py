7f7f638e0051e121045439e19f63e96f2f2a7c30
Jeremy Mock <jmock@lcls-dev3.slac.stanford.edu>
2020-04-15 13:41:50 -0700

    Add EGUL to unit changing script

--- iocBoot/common/mps_link_node_change_units.py
+++ iocBoot/common/mps_link_node_change_units.py
@@ -4,66 +4,71 @@ import sys
 import argparse
 from epics import caget, caput
 
-def change_units(PV, newHigh, newEGU, integrator=False):
+def change_units(PV, newHigh, newLow, newEGU, integrator=False):
   tempPV = '{pv}.EGU'.format(pv=PV)
   caput(tempPV,newEGU)
   if not integrator:
     tempPV1 = '{pv}.EGUF'.format(pv=PV)
     caput(tempPV1, newHigh)
+    tempPV1 = '{pv}.EGUL'.format(pv=PV)
+    caput(tempPV1, newLow)
 
 #=== MAIN ==================================================================================
 parser = argparse.ArgumentParser(description='Adjust scale and units for MPS analog inputs')
 parser.add_argument('--prefix', metavar='prefix', required=True, help="MPLN Prefix for link node")
 parser.add_argument('--ch', metavar='channel', required=True, help="MPLN Analog input channel")
 parser.add_argument('--max', metavar='MaxValue', required=True, help="Maximum Value in EGU")
+parser.add_argument('--min', metavar='MinValue', required=True, help="Minimum Value in EGU")
 parser.add_argument('--egu', metavar='EGU', required=True, help="Engineering Units (String)")
 parser.add_argument('--int',metavar="I0 or I1",required=False,help="Integrator Number (I0 or I1)")
 args = parser.parse_args()
 
 prefix = args.prefix
 maxVal = float(args.max)
+minVal = float(args.min)
 adcChannel = int(args.ch)
 units = args.egu
 integrator=args.int
 
 if not integrator:
   PV = '{p}:ADC_DATA_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal, minVal, units)
 
   maxVal = maxVal*2
+  minVal = minVal*2
 
   PV = '{p}:ANA_MIN_VAL_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal, minVal, units)
 
   PV = '{p}:ANA_AVG_VAL_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:ANA_MAX_VAL_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:LC1_ANA_FPRCVAL_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:LC1_ANA_FPRCVAL_{ch}_RBV'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:LC2_ANA_FPRCVAL_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:LC2_ANA_FPRCVAL_{ch}_RBV'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:ANA_BSA_DATA_{ch}'.format(p=prefix,ch=adcChannel)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:ANA_BSA_DATA_{ch}'.format(p=prefix,ch=adcChannel+6)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:ANA_BSA_DATA_{ch}'.format(p=prefix,ch=adcChannel+12)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
   PV = '{p}:ANA_BSA_DATA_{ch}'.format(p=prefix,ch=adcChannel+18)
-  change_units(PV, maxVal, units)
+  change_units(PV, maxVal,minVal, units)
 
 if integrator == 'I0' or integrator == 'I1':
   slope = maxVal / 65536 * 2.
