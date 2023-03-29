6441cd04b0c6c93b4fe2b749bc9f1d989d1ce006
Jeremy Mock <jeremy.a.mock@gmail.com>
2023-03-29 13:54:32 -0700

    Add new dev IOC for FWS test stand

---
+++ iocBoot/sioc-b34-mp03/st.cmd
@@ -0,0 +1,23 @@
+#!iocSpecificRelease/bin/linuxRT-x86_64/l2MpsLN
+#
+# SIOC:GUNB:MP01
+#
+
+< envPaths
+
+epicsEnvSet("SLOT_ID", "2")
+epicsEnvSet("FPGA_IP","10.0.1.10${SLOT_ID}")
+epicsEnvSet("FACILITY","dev")
+epicsEnvSet("TYPE","DN")
+
+epicsEnvSet("LOCATION","FEEH")
+epicsEnvSet("LOCATION_INDEX","MP01")
+epicsEnvSet("MODE_INPV", "MPS:UNDH:1:FACMODE CPP MSI")
+
+#
+# Loads common Link Node startup
+#
+< ${TOP}/iocBoot/common/start.cmd
+
+system("scripts/setupBPClockRT.sh shm-b34-mc01-1")
+
