45117e1370a48f5af1fe36e9d5313a6894a274c4
Jeremy Mock <jeremy.a.mock@gmail.com>
2024-04-03 10:29:29 -0700

    Add new B34 test IOC

---
+++ iocBoot/sioc-b34-mp11/st.cmd
@@ -0,0 +1,25 @@
+#!iocSpecificRelease/bin/linuxRT-x86_64/l2MpsLN
+#
+# SIOC:B34:MP11
+# shm-b34-sp08-1 slot 2
+
+< envPaths
+
+epicsEnvSet("SLOT_ID", "2")
+epicsEnvSet("L2MPS_PREFIX", "MPLN:B34:MP01:1")
+epicsEnvSet("FPGA_IP","10.0.1.10${SLOT_ID}")
+epicsEnvSet("FACILITY","dev")
+epicsEnvSet("TYPE","LN")
+epicsEnvSet("APPID","2")
+epicsEnvSet("DIGAID","1")
+
+epicsEnvSet("LOCATION","B34")
+epicsEnvSet("LOCATION_INDEX","MP11")
+epicsEnvSet("MODE_INPV", "MPS:UNDH:1:FACMODE CPP MSI")
+
+#
+# Loads common Link Node startup
+#
+< ${TOP}/iocBoot/common/start.cmd
+
+system("scripts/setupBPClockRT.sh shm-b34-sp08-1")
