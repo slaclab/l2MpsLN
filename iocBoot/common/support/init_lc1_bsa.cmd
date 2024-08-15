f9f82a4c442a3c91e1ebebe587430a5a0b29f034
Jeremy Mock <jeremy.a.mock@gmail.com>
2024-08-15 12:39:26 -0700

    Remove unused files

--- iocBoot/common/support/init_lc1_bsa.cmd
+++
@@ -1,6 +0,0 @@
-## Configure the LCLS1 BSA driver
-# L2MpsL1BsaConfig(
-#    streamName,                # Name of the CPSW stream interface for the LCLS1 BSA data
-#    recordPrefix)              # Record name prefix for the LCLS1 BSA PVs
-L2MpsL1BsaConfig("Lcls1BsaStream", "L2MPS_L1BSA")
-dbLoadRecords("db/l2MpsL1Bsa.db","P=${L2MPS_PREFIX}")
