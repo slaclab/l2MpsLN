196551d9b16a3249091ca1ec9059aa89a0872c75
Jeremy Mock <jeremy.a.mock@gmail.com>
2024-08-15 12:37:25 -0700

    Update boot script for three different types of node

--- iocBoot/common/support/init_AN.cmd
+++ iocBoot/common/support/init_AN.cmd
@@ -1,4 +1,9 @@
 # ===========================================
-# AN is the same as the LN for these
+#   First set flag that this is not a LN
+# ===========================================
+epicsEnvSet("IS_LN","#")
+
+# ===========================================
+#      Then AN is the same as the LN
 # ===========================================
 < ${TOP}/iocBoot/common/support/init_LN.cmd
