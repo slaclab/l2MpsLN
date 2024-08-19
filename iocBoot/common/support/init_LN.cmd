c44c48198ce5cc121cdd0ee8f2d468670bca9923
Jeremy Mock <jeremy.a.mock@gmail.com>
2024-08-19 07:46:19 -0700

    Remove channel number from RegisterFault command

--- iocBoot/common/support/init_LN.cmd
+++ iocBoot/common/support/init_LN.cmd
@@ -21,43 +21,43 @@ $(IS_LN="")dbLoadRecords("db/mps_soft.db","P=${L2MPS_PREFIX},PORT=${L2MPSASYN_PO
 $(CH0_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH0_P},ATTR=${CH0_R},EGU=${CH0_EGU},BAY=0,INP=0,TPR=$(TPR)")
 $(CH0_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH0_P},ATTR=${CH0_R},EGU=${CH0_EGU},BAY=0,INP=0")
 $(CH0_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH0_P},BAY=0,INP=0,GCH=${CH0_GCH}")
-$(CH0_MPS=#)L2MPSASYNRegisterFault("${CH0_P}:I0_${CH0_R}",0)
-$(CH0_MPS=#)L2MPSASYNRegisterFault("${CH0_P}:I1_${CH0_R}",6)
+$(CH0_MPS=#)L2MPSASYNRegisterFault("${CH0_P}:I0_${CH0_R}")
+$(CH0_MPS=#)L2MPSASYNRegisterFault("${CH0_P}:I1_${CH0_R}")
 
 # Channel 1
 $(CH1_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH1_P},ATTR=${CH1_R},EGU=${CH1_EGU},BAY=0,INP=1,TPR=$(TPR)")
 $(CH1_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH1_P},ATTR=${CH1_R},EGU=${CH1_EGU},BAY=0,INP=1")
 $(CH1_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH1_P},BAY=0,INP=0,GCH=${CH1_GCH}")
-$(CH1_MPS=#)L2MPSASYNRegisterFault("${CH1_P}:I0_${CH1_R}",1)
-$(CH1_MPS=#)L2MPSASYNRegisterFault("${CH1_P}:I1_${CH1_R}",7)
+$(CH1_MPS=#)L2MPSASYNRegisterFault("${CH1_P}:I0_${CH1_R}")
+$(CH1_MPS=#)L2MPSASYNRegisterFault("${CH1_P}:I1_${CH1_R}")
 
 # Channel 2
 $(CH2_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH2_P},ATTR=${CH2_R},EGU=${CH2_EGU},BAY=0,INP=2,TPR=$(TPR)")
 $(CH2_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH2_P},ATTR=${CH2_R},EGU=${CH2_EGU},BAY=0,INP=2")
 $(CH2_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH2_P},BAY=0,INP=0,GCH=${CH2_GCH}")
-$(CH2_MPS=#)L2MPSASYNRegisterFault("${CH2_P}:I0_${CH2_R}",2)
-$(CH2_MPS=#)L2MPSASYNRegisterFault("${CH2_P}:I1_${CH2_R}",8)
+$(CH2_MPS=#)L2MPSASYNRegisterFault("${CH2_P}:I0_${CH2_R}")
+$(CH2_MPS=#)L2MPSASYNRegisterFault("${CH2_P}:I1_${CH2_R}")
 
 # Channel 3
 $(CH3_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH3_P},ATTR=${CH3_R},EGU=${CH3_EGU},BAY=1,INP=0,TPR=$(TPR)")
 $(CH3_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH3_P},ATTR=${CH3_R},EGU=${CH3_EGU},BAY=1,INP=0")
 $(CH3_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH3_P},BAY=0,INP=0,GCH=${CH3_GCH}")
-$(CH3_MPS=#)L2MPSASYNRegisterFault("${CH3_P}:I0_${CH3_R}",3)
-$(CH3_MPS=#)L2MPSASYNRegisterFault("${CH3_P}:I1_${CH3_R}",9)
+$(CH3_MPS=#)L2MPSASYNRegisterFault("${CH3_P}:I0_${CH3_R}")
+$(CH3_MPS=#)L2MPSASYNRegisterFault("${CH3_P}:I1_${CH3_R}")
 
 # Channel 4
 $(CH4_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH4_P},ATTR=${CH4_R},EGU=${CH4_EGU},BAY=1,INP=1,TPR=$(TPR)")
 $(CH4_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH4_P},ATTR=${CH4_R},EGU=${CH4_EGU},BAY=1,INP=1")
 $(CH4_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH4_P},BAY=0,INP=0,GCH=${CH4_GCH}")
-$(CH4_MPS=#)L2MPSASYNRegisterFault("${CH4_P}:I0_${CH4_R}",4)
-$(CH4_MPS=#)L2MPSASYNRegisterFault("${CH4_P}:I1_${CH4_R}",10)
+$(CH4_MPS=#)L2MPSASYNRegisterFault("${CH4_P}:I0_${CH4_R}")
+$(CH4_MPS=#)L2MPSASYNRegisterFault("${CH4_P}:I1_${CH4_R}")
 
 # Channel 5
 $(CH5_MPS=#)dbLoadRecords("db/amc_ai.db","P=${CH5_P},ATTR=${CH5_R},EGU=${CH5_EGU},BAY=1,INP=2,TPR=$(TPR)")
 $(CH5_L1BSA=#)dbLoadRecords("db/l1_bsa.db","P=${CH5_P},ATTR=${CH5_R},EGU=${CH5_EGU},BAY=1,INP=2")
 $(CH5_WF=#)dbLoadRecords("db/mps_wf.db","P=${CH5_P},BAY=0,INP=0,GCH=${CH5_GCH}")
-$(CH5_MPS=#)L2MPSASYNRegisterFault("${CH5_P}:I0_${CH5_R}",5)
-$(CH5_MPS=#)L2MPSASYNRegisterFault("${CH5_P}:I1_${CH5_R}",11)
+$(CH5_MPS=#)L2MPSASYNRegisterFault("${CH5_P}:I0_${CH5_R}")
+$(CH5_MPS=#)L2MPSASYNRegisterFault("${CH5_P}:I1_${CH5_R}")
 
 set_pass0_restoreFile("ana_units.sav")
 set_pass1_restoreFile("ana_units.sav")
